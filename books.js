65b821f3773ec848ed11c092f30e50861a9e50de
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-04-26 21:14:57 +0200

    creating Sample Book Shop using Vue

---
+++ books.js
@@ -0,0 +1,60 @@
+Books = [
+    {
+        id:1,
+        name:"Book One",
+        category:"Action",
+        numberOfPages:120,
+        Author:"mohamed Salah",
+        price:300,
+        image:"https://images.unsplash.com/photo-1474932430478-367dbb6832c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmljdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+
+    },
+    {
+        id:2,
+        name:"Book Two",
+        category:"Action",
+        numberOfPages:180,
+        Author:"Salah",
+        price:350,
+        image:"https://images.unsplash.com/photo-1474932430478-367dbb6832c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmljdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+
+    },
+    {
+        id:3,
+        name:"Book Three",
+        category:"Fiction",
+        numberOfPages:220,
+        Author:"mohamed",
+        price:380,
+        image:"https://images.unsplash.com/photo-1474932430478-367dbb6832c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmljdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+
+    },
+    {
+        id:4,
+        name:"Book Four",
+        category:"Comedy",
+        numberOfPages:40,
+        Author:"mohamed Salah",
+        price:20,
+        image:"https://images.unsplash.com/photo-1474932430478-367dbb6832c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmljdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id:5,
+        name:"Book Five",
+        category:"Fiction",
+        numberOfPages:220,
+        Author:"mohamed",
+        price:380,
+        image:"https://images.unsplash.com/photo-1474932430478-367dbb6832c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmljdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+
+    },
+    {
+        id:6,
+        name:"Book six",
+        category:"Comedy",
+        numberOfPages:40,
+        Author:"mohamed Salah",
+        price:20,
+        image:"https://images.unsplash.com/photo-1474932430478-367dbb6832c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmljdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    }
+]
